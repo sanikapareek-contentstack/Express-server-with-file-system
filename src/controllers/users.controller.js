b7a2ac0fe586d3896f831f68b951ec57e0e8aa79
sanikapareek-contentstack <sanika.pareek@contentstack.com>
2023-02-09 21:30:22 +0530

    Added get user by id api

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -35,5 +35,37 @@ const getUserById=(req,res)=>{
             sendErrorResponse(new AppError("Unable to read the file",500),req,res);
         })
 }
+const validateUser=(req,res,next)=>{
+    const {body}=req;
+    const validKeys=[
+        "id",
+        "name",
+        "profileImage",
+        "introduction",
+        "profileLink"
+    ]
+    const result=validKeys.every((key)=>Object.keys(body).includes(key));
+    if(!result){
+        return sendErrorResponse(new AppError("Data Missing",422),req,res);
+    }
+    next();
+}
+const createUser=(req,res)=>{
+    readFilePromise("/Users/sanika.pareek/Desktop/Express-server-with-file-system/data/users.json")
+        .then((users)=>{
+            return JSON.parse(users);
+        })
+        .then((users)=>{
+            const{params:{id}}=req;
+            const user=users.find((user)=>user.id===id);
+            if(user){
+                return sendErrorResponse(new AppError("User with this Id already exists",404),req,res);
+            }
+            return sendResponse(req,res,{statusCode:200,message:"User added",payload:user});
+        })
+        .catch((err)=>{
+            sendErrorResponse(new AppError("Unable to read the file",500),req,res);
+        })
+}
 
-module.exports={getUsers}
+module.exports={getUsers,getUserById,validateUser,createUser}
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,7 +1,13 @@
 const express=require("express");
-const { getUsers } = require("../controllers/users.controller");
+const { 
+    getUsers, 
+    getUserById, 
+    validateUser,
+    createUser
+} = require("../controllers/users.controller");
 const router=express.Router();
 
-router.route("").get(getUsers);
+router.route("").get(getUsers).post(validateUser,createUser);
+router.route("/:id").get(getUserById);
 
 module.exports=router;
